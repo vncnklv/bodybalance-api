2fb955f5ec293642f7db740fd4dd52cefa415d4a
Ventsislav <vncnklv0@gmail.com>
2023-04-01 14:32:47 +0300

    refactor auth middleware to hook not the user's id, but the user's object to the req

--- middleware/isAuth.js
+++ middleware/isAuth.js
@@ -1,18 +1,29 @@
+const User = require("../models/User");
 const { verifyToken } = require("../utilities");
 
-exports.isAuth = (req, res, next) => {
+exports.isAuth = async (req, res, next) => {
     const token = req.headers['x-authorization'];
     const userId = verifyToken(token);
 
     if (!userId) {
         res.status(401).json({
-            status: "unauthorized",
+            status: "failed",
             message: "Please login."
         })
         return
     }
 
-    req.userId = userId;
+    const user = await User.findById(userId);
+
+    if(!user) {
+        res.status(401).json({
+            status: "failed",
+            message: "Please login."
+        })
+        return
+    }
+    
+    req.user = user; 
 
     next();
 }
