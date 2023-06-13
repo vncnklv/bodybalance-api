2522acad2ee701d4218e6d305a74900b0bf013a1
Ventsislav Nikolov <vncnklv0@gmail.com>
2023-06-13 13:18:27 +0300

    Add cors to the project

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require('express');
 require('dotenv').config();
 require("./database");
 require('./models/Diary');
-const cors = require('./middleware/cors');
+const cors = require('cors');
 const requestLogger = require("./middleware/requestLogger");
 
 const routes = require("./routes/routes");
 
 const app = express();
 
-// app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(requestLogger);
